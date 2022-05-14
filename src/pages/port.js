c3372c866378d70f4d12d7da15569c6c57f1e7c3
Jordyn Fulbright <jordynfulbright@gmail.com>
2022-05-14 02:17:19 -0400

    added final project 2022 spring to portfolio

--- src/pages/port.js
+++ src/pages/port.js
@@ -14,17 +14,17 @@ const Portfolio = () => {
                 <div class="leftPointer">
                     <div class="leftItem">
                         <div class="github">
-                            <p class="head"><em>April 2022</em></p>
+                            <p class="head"><em>May 2022</em></p>
                             <a href="https://github.com/jordynful/DramaApp-41422" activeStyle>
                                 <img src={require('../components/NavBar/githubAlt.png')} alt="GitHub logo" class="img" />
                             </a>
                         </div>
-                        <h1 class="head">TV Series App</h1>
+                        <h1 class="head">Software Developer App</h1>
                         <p class="description">
-                            Developed an app in SwiftUI for a course at the University of Georgia.
-                            The app allows you to store your TV dramas, with information
-                            about them such as your watched status, how many episodes you’ve seen, and any
-                            notes about the program.</p>
+                            Developed an app in React Native for a course at UGA. The app implements user-authentication using MongoDB as a databse. The app would be
+                            for requesting services from a software
+                            developer(me). The user can scroll through the offered services or use the search function.
+                            Each service when clicked on, takes the user to a detailed page about the service.</p>
                     </div>
 
                     <div class="leftItem">
@@ -42,8 +42,58 @@ const Portfolio = () => {
                     </div>
                     <div class="leftItem">
                         <div class="github">
-                            <p class="head"><em>December 2021</em></p>
+                            <p class="head"><em>February 2022</em></p>
                             <a href="https://github.com/jordynful/cs1730p1" activeStyle>
+                                <img src={require('../components/NavBar/githubAlt.png')} alt="GitHub logo" class="img" />
+                            </a>
+                        </div>
+                        <h1 class="head">Strive</h1>
+                        <p class="description">
+                            Created a website for the UGA Hackathon. The website was intended
+                            to act as a platform between contractors and clients for “odd job” type work. During this
+                            project, I learned html, css, and Javascript.
+                        </p>
+                    </div>
+                </div>
+                <div class="chron">
+                    <div class="vertLine"></div>
+                </div>
+                <div class="rightPointer">
+                    <div class="leftItem">
+                        <div class="github">
+                            <p class="head"><em>April 2022</em></p>
+                            <a href="https://github.com/jordynful/DSp3" activeStyle>
+                                <img src={require('../components/NavBar/githubAlt.png')} alt="GitHub logo" class="img" />
+                            </a>
+                        </div>
+                        <h1 class="head">TV Series App</h1>
+                        <p class="description">
+                            Developed an app in SwiftUI for a course at the University of Georgia.
+                            The app allows you to store your TV dramas, with information
+                            about them such as your watched status, how many episodes you’ve seen, and any
+                            notes about the program.
+                        </p>
+                    </div>
+                    <div class="leftItem">
+                        <div class="github">
+                            <p class="head"><em>March 2022</em></p>
+                            <a href="https://github.com/jordynful/IndustRealize" activeStyle>
+                                <img src={require('../components/NavBar/githubAlt.png')} alt="GitHub logo" class="img" />
+                            </a>
+                        </div>
+                        <h1 class="head">Binary Search Tree</h1>
+                        <p class="description">
+                            Wrote a C++ program that creates a Binary Search Tree from
+                            an input file of values. The program corrects the tree according
+                            to any insertions or deletions during runtime. The program includes a
+                            variety of commands, for example, the user can request to
+                            get the sum of all values in a subtree.
+                        </p>
+                    </div>
+                    <div class="leftItem">
+                        <div class="github">
+                            <p class="head"><em>December 2021</em></p>
+                            <a href="https://github.com/jordynful/IndustRealize" activeStyle>
                                 <img src={require('../components/NavBar/githubAlt.png')} alt="GitHub logo" class="img" />
                             </a>
                         </div>
@@ -54,41 +104,6 @@ const Portfolio = () => {
                             forks and their parent/child processes as well as system calls within
                             a program.
                         </p>
-                    </div>
-                </div>
-                <div class="chron">
-                    <div class="vertLine"></div>
-                </div>
-                <div class="rightPointer">
-                    <div class="leftItem">
-                        <div class="github">
-                            <p class="head"><em>March 2022</em></p>
-                            <a href="https://github.com/jordynful/DSp3" activeStyle>
-                                <img src={require('../components/NavBar/githubAlt.png')} alt="GitHub logo" class="img" />
-                            </a>
-                        </div>
-                        <h1 class="head">Binary Search Tree</h1>
-                        <p class="description">
-                            Wrote a C++ program that creates a Binary Search Tree from
-                            an input file of values. The program corrects the tree according
-                            to any insertions or deletions during runtime. The program includes a 
-                            variety of commands, for example, the user can request to
-                             get the sum of all values in a subtree.
-                        </p>
-                    </div>
-                    <div class="leftItem">
-                        <div class="github">
-                            <p class="head"><em>February 2022</em></p>
-                            <a href="https://github.com/jordynful/IndustRealize" activeStyle>
-                                <img src={require('../components/NavBar/githubAlt.png')} alt="GitHub logo" class="img" />
-                            </a>
-                        </div>
-                        <h1 class="head">Strive</h1>
-                        <p class="description">
-                            Created a website for the UGA Hackathon. The website was intended
-                            to act as a platform between contractors and clients for “odd job” type work. During this                 
-                         project, I learned html, css, and Javascript. 
-                            </p>
                     </div>
 
                 </div>
